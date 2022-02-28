6e56028b329044d7e4f4b068222aec05378ed271
Dominik Zogg <dominik.zogg@hotmail.com>
2022-02-28 17:39:02 +0100

    php 8.0 min version

--- README.md
+++ README.md
@@ -26,8 +26,8 @@ Symfony Router implementation for [chubbyphp-framework][1].
 
 ## Requirements
 
- * php: ^7.4|^8.0
- * [chubbyphp/chubbyphp-framework][1]: ^3.6.2|^4.0@dev
+ * php: ^8.0
+ * [chubbyphp/chubbyphp-framework][1]: ^4.0
  * [psr/http-message][2]: ^1.0.1
  * [symfony/expression-language][3]: ^4.4.34|^5.3.11|^6.0.0
  * [symfony/routing][4]: ^4.4.34|^5.3.11|^6.0.0
@@ -37,7 +37,7 @@ Symfony Router implementation for [chubbyphp-framework][1].
 Through [Composer](http://getcomposer.org) as [chubbyphp/chubbyphp-framework-router-symfony][10].
 
 ```bash
-composer require chubbyphp/chubbyphp-framework-router-symfony "^1.2"
+composer require chubbyphp/chubbyphp-framework-router-symfony "^1.3"
 ```
 
 ## Usage
@@ -83,7 +83,7 @@ $app->emit($app->handle((new ServerRequestFactory())->createFromGlobals()));
 
 ## Copyright
 
-Dominik Zogg 2021
+Dominik Zogg 2022
 
 [1]: https://packagist.org/packages/chubbyphp/chubbyphp-framework
 [2]: https://packagist.org/packages/psr/http-message
--- src/Router.php
+++ src/Router.php
@@ -2,255 +2,238 @@
 
 declare(strict_types=1);
 
-namespace Chubbyphp\Framework\Router {
-    if (!interface_exists('RouteInterface')) {
-        interface RouterInterface extends RouteMatcherInterface, UrlGeneratorInterface
-        {
-        }
-    }
-}
+namespace Chubbyphp\Framework\Router\Symfony;
 
-namespace Chubbyphp\Framework\Router\Symfony {
-    use Chubbyphp\Framework\Router\Exceptions\MethodNotAllowedException;
-    use Chubbyphp\Framework\Router\Exceptions\MissingRouteByNameException;
-    use Chubbyphp\Framework\Router\Exceptions\NotFoundException;
-    use Chubbyphp\Framework\Router\Exceptions\RouteGenerationException;
-    use Chubbyphp\Framework\Router\RouteInterface;
-    use Chubbyphp\Framework\Router\RouterInterface;
-    use Psr\Http\Message\ServerRequestInterface;
-    use Symfony\Component\Routing\Exception\InvalidParameterException as SymfonyInvalidParameterException;
-    use Symfony\Component\Routing\Exception\MethodNotAllowedException as SymfonyMethodNotAllowedException;
-    use Symfony\Component\Routing\Exception\MissingMandatoryParametersException as SymfonyMissingMandatoryParametersException;
-    use Symfony\Component\Routing\Exception\ResourceNotFoundException as SymfonyResourceNotFoundException;
-    use Symfony\Component\Routing\Generator\CompiledUrlGenerator;
-    use Symfony\Component\Routing\Generator\Dumper\CompiledUrlGeneratorDumper;
-    use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
-    use Symfony\Component\Routing\Matcher\CompiledUrlMatcher;
-    use Symfony\Component\Routing\Matcher\Dumper\CompiledUrlMatcherDumper;
-    use Symfony\Component\Routing\RequestContext;
-    use Symfony\Component\Routing\Route;
-    use Symfony\Component\Routing\RouteCollection;
+use Chubbyphp\Framework\Router\Exceptions\MethodNotAllowedException;
+use Chubbyphp\Framework\Router\Exceptions\MissingRouteByNameException;
+use Chubbyphp\Framework\Router\Exceptions\NotFoundException;
+use Chubbyphp\Framework\Router\Exceptions\RouteGenerationException;
+use Chubbyphp\Framework\Router\RouteInterface;
+use Chubbyphp\Framework\Router\RouteMatcherInterface;
+use Chubbyphp\Framework\Router\UrlGeneratorInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Symfony\Component\Routing\Exception\InvalidParameterException as SymfonyInvalidParameterException;
+use Symfony\Component\Routing\Exception\MethodNotAllowedException as SymfonyMethodNotAllowedException;
+use Symfony\Component\Routing\Exception\MissingMandatoryParametersException as SymfonyMissingMandatoryParametersException;
+use Symfony\Component\Routing\Exception\ResourceNotFoundException as SymfonyResourceNotFoundException;
+use Symfony\Component\Routing\Generator\CompiledUrlGenerator;
+use Symfony\Component\Routing\Generator\Dumper\CompiledUrlGeneratorDumper;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface as SymfonyUrlGeneratorInterface;
+use Symfony\Component\Routing\Matcher\CompiledUrlMatcher;
+use Symfony\Component\Routing\Matcher\Dumper\CompiledUrlMatcherDumper;
+use Symfony\Component\Routing\RequestContext;
+use Symfony\Component\Routing\Route;
+use Symfony\Component\Routing\RouteCollection;
 
-    final class Router implements RouterInterface
+final class Router implements RouteMatcherInterface, UrlGeneratorInterface
+{
+    public const PATH_DEFAULTS = 'defaults';
+    public const PATH_REQUIREMENTS = 'requirements';
+    public const PATH_HOST = 'host';
+    public const PATH_SCHEMES = 'schemes';
+    public const PATH_CONDITION = 'condition';
+
+    private const MATCHER = 'matcher';
+    private const GENERATOR = 'generator';
+
+    /**
+     * @var array<string, RouteInterface>
+     */
+    private array $routesByName;
+
+    private CompiledUrlMatcher $urlMatcher;
+
+    private CompiledUrlGenerator $urlGenerator;
+
+    /**
+     * @param array<int, RouteInterface> $routes
+     */
+    public function __construct(array $routes, ?string $cacheFile = null, private string $basePath = '')
     {
-        public const PATH_DEFAULTS = 'defaults';
-        public const PATH_REQUIREMENTS = 'requirements';
-        public const PATH_HOST = 'host';
-        public const PATH_SCHEMES = 'schemes';
-        public const PATH_CONDITION = 'condition';
+        $this->routesByName = $this->getRoutesByName($routes);
 
-        private const MATCHER = 'matcher';
-        private const GENERATOR = 'generator';
+        $compiledRoutes = $this->getCompiledRoutes($routes, $cacheFile);
 
-        /**
-         * @var array<string, RouteInterface>
-         */
-        private array $routesByName;
+        $this->urlMatcher = new CompiledUrlMatcher($compiledRoutes[self::MATCHER], $this->getRequestContext());
+        $this->urlGenerator = new CompiledUrlGenerator($compiledRoutes[self::GENERATOR], $this->getRequestContext());
+    }
 
-        private CompiledUrlMatcher $urlMatcher;
+    public function match(ServerRequestInterface $request): RouteInterface
+    {
+        $this->urlMatcher->setContext($this->getRequestContext($request));
 
-        private CompiledUrlGenerator $urlGenerator;
-
-        private string $basePath;
-
-        /**
-         * @param array<int, RouteInterface> $routes
-         */
-        public function __construct(array $routes, ?string $cacheFile = null, string $basePath = '')
-        {
-            $this->routesByName = $this->getRoutesByName($routes);
-            $this->basePath = $basePath;
-
-            $compiledRoutes = $this->getCompiledRoutes($routes, $cacheFile);
-
-            $this->urlMatcher = new CompiledUrlMatcher($compiledRoutes[self::MATCHER], $this->getRequestContext());
-            $this->urlGenerator = new CompiledUrlGenerator($compiledRoutes[self::GENERATOR], $this->getRequestContext());
-        }
-
-        public function match(ServerRequestInterface $request): RouteInterface
-        {
-            $this->urlMatcher->setContext($this->getRequestContext($request));
-
-            try {
-                $parameters = $this->urlMatcher->match($request->getUri()->getPath());
-            } catch (SymfonyResourceNotFoundException $exception) {
-                throw NotFoundException::create($request->getRequestTarget());
-            } catch (SymfonyMethodNotAllowedException $exception) {
-                throw MethodNotAllowedException::create(
-                    $request->getRequestTarget(),
-                    $request->getMethod(),
-                    $exception->getAllowedMethods()
-                );
-            }
-
-            /** @var RouteInterface $route */
-            $route = $this->routesByName[$parameters['_route']];
-
-            unset($parameters['_route']);
-
-            return $route->withAttributes($parameters);
-        }
-
-        /**
-         * @param array<string, string> $attributes
-         * @param array<string, mixed>  $queryParams
-         */
-        public function generateUrl(
-            ServerRequestInterface $request,
-            string $name,
-            array $attributes = [],
-            array $queryParams = []
-        ): string {
-            $route = $this->getRoute($name);
-
-            return $this->generate($request, $name, $route->getPath(), $attributes, $queryParams);
-        }
-
-        /**
-         * @param array<string, string> $attributes
-         * @param array<string, mixed>  $queryParams
-         */
-        public function generatePath(string $name, array $attributes = [], array $queryParams = []): string
-        {
-            $route = $this->getRoute($name);
-
-            return $this->generate(null, $name, $route->getPath(), $attributes, $queryParams);
-        }
-
-        private function getRoute(string $name): RouteInterface
-        {
-            if (!isset($this->routesByName[$name])) {
-                throw MissingRouteByNameException::create($name);
-            }
-
-            return $this->routesByName[$name];
-        }
-
-        /**
-         * @param array<string, string> $attributes
-         * @param array<string, mixed>  $queryParams
-         */
-        private function generate(
-            ?ServerRequestInterface $request,
-            string $name,
-            string $path,
-            array $attributes = [],
-            array $queryParams = []
-        ): string {
-            if (!isset($this->routesByName[$name])) {
-                throw MissingRouteByNameException::create($name);
-            }
-
-            $this->urlGenerator->setContext($this->getRequestContext($request));
-
-            try {
-                return $this->urlGenerator->generate(
-                    $name,
-                    array_merge($attributes, $queryParams),
-                    null !== $request ? UrlGeneratorInterface::ABSOLUTE_URL : UrlGeneratorInterface::ABSOLUTE_PATH
-                );
-            } catch (SymfonyMissingMandatoryParametersException $exception) {
-                throw RouteGenerationException::create(
-                    $name,
-                    $path,
-                    $attributes,
-                    $exception
-                );
-            } catch (SymfonyInvalidParameterException $exception) {
-                throw RouteGenerationException::create(
-                    $name,
-                    $path,
-                    $attributes,
-                    $exception
-                );
-            }
-        }
-
-        /**
-         * @param array<int, RouteInterface> $routes
-         *
-         * @return array<mixed>
-         */
-        private function getCompiledRoutes(array $routes, ?string $cacheFile): array
-        {
-            if (null !== $cacheFile && file_exists($cacheFile)) {
-                $compiledRoutes = require $cacheFile;
-            } else {
-                $routeCollection = $this->getRouteCollection($routes);
-
-                $compiledRoutes = [
-                    self::MATCHER => (new CompiledUrlMatcherDumper($routeCollection))->getCompiledRoutes(),
-                    self::GENERATOR => (new CompiledUrlGeneratorDumper($routeCollection))->getCompiledRoutes(),
-                ];
-
-                if (null !== $cacheFile) {
-                    file_put_contents($cacheFile, '<?php return '.var_export($compiledRoutes, true).';');
-                }
-            }
-
-            return $compiledRoutes;
-        }
-
-        /**
-         * @param array<int, RouteInterface> $routes
-         */
-        private function getRouteCollection(array $routes): RouteCollection
-        {
-            $routeCollection = new RouteCollection();
-
-            foreach ($routes as $route) {
-                $pathOptions = $route->getPathOptions();
-                $routeCollection->add($route->getName(), new Route(
-                    $route->getPath(),
-                    $pathOptions[self::PATH_DEFAULTS] ?? [],
-                    $pathOptions[self::PATH_REQUIREMENTS] ?? [],
-                    [],
-                    $pathOptions[self::PATH_HOST] ?? null,
-                    $pathOptions[self::PATH_SCHEMES] ?? [],
-                    [$route->getMethod()],
-                    $pathOptions[self::PATH_CONDITION] ?? null
-                ));
-            }
-
-            return $routeCollection;
-        }
-
-        private function getRequestContext(?ServerRequestInterface $request = null): RequestContext
-        {
-            if (null === $request) {
-                return new RequestContext($this->basePath);
-            }
-
-            $uri = $request->getUri();
-
-            $scheme = $uri->getScheme();
-            $port = $uri->getPort();
-
-            return new RequestContext(
-                $this->basePath,
+        try {
+            $parameters = $this->urlMatcher->match($request->getUri()->getPath());
+        } catch (SymfonyResourceNotFoundException $exception) {
+            throw NotFoundException::create($request->getRequestTarget());
+        } catch (SymfonyMethodNotAllowedException $exception) {
+            throw MethodNotAllowedException::create(
+                $request->getRequestTarget(),
                 $request->getMethod(),
-                $uri->getHost(),
-                $scheme,
-                'http' === $scheme && null !== $port ? $port : 80,
-                'https' === $scheme && null !== $port ? $port : 443,
-                $uri->getPath(),
-                $uri->getQuery()
+                $exception->getAllowedMethods()
             );
         }
 
-        /**
-         * @param array<int, RouteInterface> $routes
-         *
-         * @return array<string, RouteInterface>
-         */
-        private function getRoutesByName(array $routes): array
-        {
-            $routesByName = [];
-            foreach ($routes as $route) {
-                $routesByName[$route->getName()] = $route;
-            }
+        /** @var RouteInterface $route */
+        $route = $this->routesByName[$parameters['_route']];
 
-            return $routesByName;
+        unset($parameters['_route']);
+
+        return $route->withAttributes($parameters);
+    }
+
+    /**
+     * @param array<string, string> $attributes
+     * @param array<string, mixed>  $queryParams
+     */
+    public function generateUrl(
+        ServerRequestInterface $request,
+        string $name,
+        array $attributes = [],
+        array $queryParams = []
+    ): string {
+        $route = $this->getRoute($name);
+
+        return $this->generate($request, $name, $route->getPath(), $attributes, $queryParams);
+    }
+
+    /**
+     * @param array<string, string> $attributes
+     * @param array<string, mixed>  $queryParams
+     */
+    public function generatePath(string $name, array $attributes = [], array $queryParams = []): string
+    {
+        $route = $this->getRoute($name);
+
+        return $this->generate(null, $name, $route->getPath(), $attributes, $queryParams);
+    }
+
+    private function getRoute(string $name): RouteInterface
+    {
+        if (!isset($this->routesByName[$name])) {
+            throw MissingRouteByNameException::create($name);
         }
+
+        return $this->routesByName[$name];
+    }
+
+    /**
+     * @param array<string, string> $attributes
+     * @param array<string, mixed>  $queryParams
+     */
+    private function generate(
+        ?ServerRequestInterface $request,
+        string $name,
+        string $path,
+        array $attributes = [],
+        array $queryParams = []
+    ): string {
+        if (!isset($this->routesByName[$name])) {
+            throw MissingRouteByNameException::create($name);
+        }
+
+        $this->urlGenerator->setContext($this->getRequestContext($request));
+
+        try {
+            return $this->urlGenerator->generate(
+                $name,
+                array_merge($attributes, $queryParams),
+                null !== $request ? SymfonyUrlGeneratorInterface::ABSOLUTE_URL : SymfonyUrlGeneratorInterface::ABSOLUTE_PATH
+            );
+        } catch (SymfonyMissingMandatoryParametersException|SymfonyInvalidParameterException $exception) {
+            throw RouteGenerationException::create(
+                $name,
+                $path,
+                $attributes,
+                $exception
+            );
+        }
+    }
+
+    /**
+     * @param array<int, RouteInterface> $routes
+     *
+     * @return array<mixed>
+     */
+    private function getCompiledRoutes(array $routes, ?string $cacheFile): array
+    {
+        if (null !== $cacheFile && file_exists($cacheFile)) {
+            $compiledRoutes = require $cacheFile;
+        } else {
+            $routeCollection = $this->getRouteCollection($routes);
+
+            $compiledRoutes = [
+                self::MATCHER => (new CompiledUrlMatcherDumper($routeCollection))->getCompiledRoutes(),
+                self::GENERATOR => (new CompiledUrlGeneratorDumper($routeCollection))->getCompiledRoutes(),
+            ];
+
+            if (null !== $cacheFile) {
+                file_put_contents($cacheFile, '<?php return '.var_export($compiledRoutes, true).';');
+            }
+        }
+
+        return $compiledRoutes;
+    }
+
+    /**
+     * @param array<int, RouteInterface> $routes
+     */
+    private function getRouteCollection(array $routes): RouteCollection
+    {
+        $routeCollection = new RouteCollection();
+
+        foreach ($routes as $route) {
+            $pathOptions = $route->getPathOptions();
+            $routeCollection->add($route->getName(), new Route(
+                $route->getPath(),
+                $pathOptions[self::PATH_DEFAULTS] ?? [],
+                $pathOptions[self::PATH_REQUIREMENTS] ?? [],
+                [],
+                $pathOptions[self::PATH_HOST] ?? null,
+                $pathOptions[self::PATH_SCHEMES] ?? [],
+                [$route->getMethod()],
+                $pathOptions[self::PATH_CONDITION] ?? null
+            ));
+        }
+
+        return $routeCollection;
+    }
+
+    private function getRequestContext(?ServerRequestInterface $request = null): RequestContext
+    {
+        if (null === $request) {
+            return new RequestContext($this->basePath);
+        }
+
+        $uri = $request->getUri();
+
+        $scheme = $uri->getScheme();
+        $port = $uri->getPort();
+
+        return new RequestContext(
+            $this->basePath,
+            $request->getMethod(),
+            $uri->getHost(),
+            $scheme,
+            'http' === $scheme && null !== $port ? $port : 80,
+            'https' === $scheme && null !== $port ? $port : 443,
+            $uri->getPath(),
+            $uri->getQuery()
+        );
+    }
+
+    /**
+     * @param array<int, RouteInterface> $routes
+     *
+     * @return array<string, RouteInterface>
+     */
+    private function getRoutesByName(array $routes): array
+    {
+        $routesByName = [];
+        foreach ($routes as $route) {
+            $routesByName[$route->getName()] = $route;
+        }
+
+        return $routesByName;
     }
 }
